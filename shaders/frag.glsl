7d528506269ee5f10eef1e41a7ffa0928d56aab9
artamonovoleg <artamonovav2003@gmail.com>
2021-04-18 22:15:48 +0300

    Checkerboard

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -2,8 +2,67 @@
 
 uniform vec2 u_resolution;
 uniform float u_time;
-
 out vec4 frag_color;
+
+const int BIT_COUNT = 8;
+
+int modi(int x, int y) 
+{
+	return x - y * (x / y);
+}
+
+int or(int a, int b) {
+    int result = 0;
+    int n = 1;
+
+    for (int i = 0; i < BIT_COUNT; i++) 
+	{
+        if ((modi(a, 2) == 1) || (modi(b, 2) == 1)) 
+            result += n;
+        a = a / 2;
+        b = b / 2;
+        n = n * 2;
+
+        if (!(a > 0 || b > 0))
+            break;
+    }
+    return result;
+}
+
+int and(int a, int b) 
+{
+    int result = 0;
+    int n = 1;
+
+    for (int i = 0; i < BIT_COUNT; i++) 
+	{
+        if ((modi(a, 2) == 1) && (modi(b, 2) == 1))
+            result += n;
+
+        a = a / 2;
+        b = b / 2;
+        n = n * 2;
+
+        if (!(a > 0 && b > 0))
+            break;
+    }
+    return result;
+}
+
+int not(int a) 
+{
+    int result = 0;
+    int n = 1;
+    
+    for (int i = 0; i < BIT_COUNT; i++) 
+	{
+        if (modi(a, 2) == 0) 
+            result += n;    
+        a = a / 2;
+        n = n * 2;
+    }
+    return result;
+}
 
 struct Ray
 {
@@ -46,13 +105,74 @@ const int MAX_HIT_DISTANCE = 1000;
 const int SPHERES_COUNT = 2;
 Sphere spheres[SPHERES_COUNT];
 
-const int LIGHTS_COUNT = 2;
+const int LIGHTS_COUNT = 1;
 Light lights[LIGHTS_COUNT];
 
+// need to find nearest object
+float min_hit_distance = MAX_HIT_DISTANCE;
 
 vec3 CalculateLight(Light light, Sphere sphere, vec3 normal)
 {
 	return dot(normalize(light.position - sphere.center), normal) * light.color;
+}
+
+vec3 CheckerBoard(Ray ray, vec3 previous_color)
+{
+	if (abs(ray.direction.y) > 1 * pow(10, -3))
+	{
+		float d = -(ray.origin.y + 4.0) / ray.direction.y; // the checkerboard plane has equation y = -4
+        vec3 pt = ray.origin + ray.direction * d;
+
+        if (d > 0.0 && abs(pt.x) < 10.0 && pt.z < -10.0 && pt.z >- 30.0) 
+		{
+            float checkerboard_dist = d;
+			if (checkerboard_dist > min_hit_distance)
+				return previous_color;
+
+            vec3 hit = pt;
+            vec3 N = vec3(0.0, 1.0, 0.0);
+
+			int val = int(0.5 * hit.x + 1000.0) + int(0.5 * hit.z);
+
+			if (and(val, 1) == 1)
+				return vec3(1.0) * 0.3;
+			else
+				return vec3(1.0, 0.7, 0.3) * 0.3;
+        }
+	}
+
+	return previous_color;
+}
+
+vec3 SceneIntersect(Ray ray)
+{
+	vec3 color = vec3(0.2, 0.7, 0.8);
+
+	// Spheres intersection
+	for (int s = 0; s < SPHERES_COUNT; ++s)
+	{
+		float t = HitSphere(ray, spheres[s]);
+
+		if (t > 0.0 && t < min_hit_distance)
+		{
+			vec3 hit = ray.origin + t * ray.direction;
+			vec3 N = normalize(hit - spheres[s].center);
+
+			vec3 diffuse_light = vec3(0.0);
+
+			for (int l = 0; l < LIGHTS_COUNT; ++l)
+				diffuse_light += CalculateLight(lights[l], spheres[s], N);
+			
+			color = spheres[s].material.color * diffuse_light;
+
+			min_hit_distance = t;
+		}
+	}
+
+	// Draw checkerboard
+	color = CheckerBoard(ray, color);
+
+	return color;
 }
 
 void main()
@@ -81,47 +201,17 @@ void main()
 	/// Create scene light
 	Light light;
 	light.position = vec3(0.0);
-	light.color = vec3(1.0, 0.0, 0.0);
+	light.color = vec3(1.0);
 	lights[0] = light;
-
-	light.color = vec3(0.0, 0.0, 1.0);
-	lights[1] = light;
 	///
-
-	vec3 color;
 
 	// Create ray
 	Ray ray;
 	ray.origin = vec3(0.0, 0.0, 0.0);
 	ray.direction = normalize(vec3(coord, -1.0));
 
-	// need to find nearest object
-	float min_hit_distance = MAX_HIT_DISTANCE;
-
 	// change light position for better understanding
 	lights[0].position = vec3(cos(u_time), 0.0, 0.0);
-	lights[1].position = vec3(sin(u_time), 0.0, 0.0);
 
-	for (int s = 0; s < SPHERES_COUNT; ++s)
-	{
-		float t = HitSphere(ray, spheres[s]);
-
-		if (t > 0.0 && t < min_hit_distance)
-		{
-			vec3 hit = ray.origin + t * ray.direction;
-			vec3 N = normalize(hit - spheres[s].center);
-
-			vec3 diffuse_light = vec3(0.0);
-
-			for (int l = 0; l < LIGHTS_COUNT; ++l)
-				diffuse_light += CalculateLight(lights[l], spheres[s], N);
-			
-			color = spheres[s].material.color * diffuse_light;
-
-			min_hit_distance = t;
-		}
-	}
-
-
-	frag_color = vec4(color, 1.0);
+	frag_color = vec4(SceneIntersect(ray), 1.0);
 }
